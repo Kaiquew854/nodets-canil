0285f85649b8a00fec95bd6e3a1ec0a5b738f357
kaique winicios <kaiquew@outlook.com>
2022-12-07 16:52:22 -0300

    base criada

---
+++ src/server.ts
@@ -0,0 +1,27 @@
+import  Express  from "express";
+import dotenv from 'dotenv';
+import mustache from 'mustache-express';
+import path from 'path';
+import mainRoutes from './routes/index'
+
+
+dotenv.config();
+
+const server = Express();
+
+server.set('view engine', 'mustache'); //qual engine de visualização
+server.set('views', path.join(__dirname, 'views')); //diretório padra a pasta views
+server.engine('mustache', mustache()); //roda função da engine
+
+server.use(Express.static(path.join(__dirname, '../public'))); //diretorio das paginas estáticas
+
+// rotas
+
+//erro 404
+server.use(mainRoutes, (req, res)=>{
+    res.status(404).send('Sorry, page not found');
+})
+
+
+
+server.listen(process.env.PORT);
